6835070be68aa778e13c8fad8926d860b39bd207
hh404 <hjw231_2003@sina.com>
2026-02-19 19:40:10 +0800

    Initial Commit

---
+++ FlipShot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlipShot
+//
+//  Created by hanshuang on 2026/2/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlipShotTests/FlipShotTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlipShotTests.swift
+//  FlipShotTests
+//
+//  Created by hanshuang on 2026/2/19.
+//
+
+import Testing
+@testable import FlipShot
+
+struct FlipShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
